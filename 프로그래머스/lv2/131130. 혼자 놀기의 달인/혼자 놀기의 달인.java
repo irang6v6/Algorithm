4b749bf47f6e9c32365d808890ff27a4debf1086
남이랑 <42470768+irang6v6@users.noreply.github.com>
2022-10-28 21:38:23 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 1.30 ms, Memory: 84.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
@@ -0,0 +1,70 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+	static int totalCards, maxScore;
+	static boolean[] opened;
+	static int[] card;
+
+	public static int solution(int[] cards) {
+
+		for (int i = 0; i < cards.length; i++) {
+			cards[i] -= 1;
+		}
+
+		card = cards;
+//		System.out.println(Arrays.toString(card));
+		totalCards = cards.length;
+		opened = new boolean[totalCards];
+		maxScore = Integer.MIN_VALUE;
+
+		// 랜덤으로 두 장 뽑아서 게임 진행
+		int[] pick = new int[2];
+		comb(0, 0, pick);
+
+		return maxScore;
+	}
+
+	private static void comb(int start, int cnt, int[] pick) {
+		if (cnt == 2) {
+//			System.out.println(Arrays.toString(pick));
+			play(pick);
+			return;
+		}
+
+		for (int i = start; i < totalCards; i++) {
+			pick[cnt] = i;
+			comb(i, cnt + 1, pick);
+		}
+
+	}
+
+	private static void play(int[] pick) {
+		// 게임
+//		System.out.println(Arrays.toString(pick));
+		int[] score = { 0, 0 };
+		opened = new boolean[totalCards + 1];
+		for (int i = 0; i < 2; i++) {
+			int cur = pick[i];
+			if (!opened[card[cur]])
+				score[i]++;
+//			System.out.println("firstPick: " + cur);
+			while (!opened[cur]) {
+				opened[cur] = true;
+				int next = card[cur];
+				if (opened[next])
+					break;
+				cur = next;
+//				System.out.println("Pick: " + cur);
+				score[i]++;
+			}
+		}
+		int gameScore = score[0] * score[1];
+//		System.out.println(gameScore);
+		maxScore = gameScore > maxScore ? gameScore : maxScore;
+
+	}
+
+
+}
