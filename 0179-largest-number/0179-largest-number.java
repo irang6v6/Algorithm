ba3e7bfd2f86646286d1df782cb6d74750bd4104
남이랑 <42470768+irang6v6@users.noreply.github.com>
2022-12-20 03:09:47 +0900

    Time: 16 ms (20.07%), Space: 45.6 MB (9.01%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.java
@@ -0,0 +1,24 @@
+import java.util.PriorityQueue;
+
+public class Solution {
+    public static String largestNumber(int[] nums) {
+        PriorityQueue<String> priorityQueue = new PriorityQueue<>((o1, o2) -> Long.parseLong(o1 + o2) > Long.parseLong(o2 + o1) ? -1 : 1);
+
+        for (int i : nums) {
+            priorityQueue.add(Long.toString(i));
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!priorityQueue.isEmpty()) {
+            sb.append(priorityQueue.poll());
+        }
+
+        String answer = sb.toString();
+        if (answer.charAt(0) == '0') {
+            return "0";
+        } else {
+            return answer;
+        }
+    }
+
+}
