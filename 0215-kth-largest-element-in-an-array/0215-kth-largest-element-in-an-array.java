7064c1e5f81c836f0810478e5902b50269ae050e
남이랑 <42470768+irang6v6@users.noreply.github.com>
2023-01-05 12:56:27 +0900

    Time: 5 ms (98.45%), Space: 78 MB (8.98%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,19 +1,16 @@
-import java.util.TreeMap;
-import java.util.Map;
-
 class Solution {
     public static int findKthLargest(int[] nums, int k) {
-        Map<Integer, Integer> map = new TreeMap<>();
+        int[] count = new int[20001];
         for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            count[nums[i] + 10000]++;
         }
-        
+
         int index = 0;
         int order = nums.length - k + 1;
-        for (int key : map.keySet()) {
-            index += map.get(key);
+        for (int i = 0; i < count.length; i++) {
+            index += count[i];
             if (index >= order) {
-                return key;
+                return i - 10000;
             }
         }
         return 0;
