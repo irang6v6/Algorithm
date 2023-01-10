d1fc0db01f616c44260f1a8992ef129c9a424f5a
남이랑 <42470768+irang6v6@users.noreply.github.com>
2023-01-10 21:47:16 +0900

    Update 시험 감독.java

--- 백준/Bronze/13458. 시험 감독/시험 감독.java
+++ 백준/Bronze/13458. 시험 감독/시험 감독.java
@@ -16,19 +16,15 @@ public class Main {
         int B = Integer.parseInt(s[0]);
         int C = Integer.parseInt(s[1]);
 
-//        System.out.println(Arrays.toString(rooms));
-//        System.out.println("시험장 수: "+N+" 총 감독관: "+B+" 부감독관: "+C);
-
         long answer = 0;
         for (int i = 0; i < rooms.length; i++) {
             long needC = 0;
-//            System.out.println("총감동"+rooms[i]);
             rooms[i] -= B;
-//            System.out.println("총감독 후"+rooms[i]);
+            
             if (rooms[i] <= 0) continue;
+            
             needC += rooms[i] / C;
             if (rooms[i] % C > 0) needC += 1;
-//            System.out.println("need = " + needC);;
             answer += needC;
         }
         answer += rooms.length;
