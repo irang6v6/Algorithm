f89244f4a1c4c0fd97786eb6c2dfd2d522365f61
남이랑 <42470768+irang6v6@users.noreply.github.com>
2023-01-25 19:33:52 +0900

    Time: 40 ms (36.32%), Space: 42.5 MB (91.50%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+     public static int jump(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length && j <= i + nums[i]; j++) {
+                if (dp[j] > dp[i] + 1) {
+                    dp[j] = dp[i] + 1;
+                }
+            }
+        }
+        return dp[nums.length - 1];
+    }
+}
