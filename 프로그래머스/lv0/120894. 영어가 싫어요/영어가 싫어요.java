801b0092975ef3c54a2073c0498b9c8af41038c3
남이랑 <irang6v6@gmail.com>
2023-04-12 23:41:25 +0900

    [level 0] Title: 영어가 싫어요, Time: 7.02 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public long solution(String numbers) {
+        long answer = 0;
+        String[] num = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        
+    
+    for(int i = 0; i < num.length; i++){
+        if(numbers.length() >= num[i].length()){
+            numbers = numbers.replaceAll(num[i],i+"");
+        } 
+    }
+    return Long.parseLong(numbers);
+    }
+}
