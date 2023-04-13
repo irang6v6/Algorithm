8a505e5d89e43b1f743f75f1abd0ecaaa5b3dc89
남이랑 <irang6v6@gmail.com>
2023-04-13 23:57:09 +0900

    [level 1] Title: 시저 암호, Time: 1.70 ms, Memory: 94.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String solution(String s, int n) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        for(Character c : s.toCharArray()){
+            if(c==' '){
+                sb.append(' ');
+            } else {                
+                int num = c + n;
+                if(c >= 65 && c <= 90){ //A~Z
+                    if(num > 90){
+                        num = num - 26;
+                    }
+                }else if(c >= 97 && c <= 122){ //a~z
+                    if(num > 122){
+                        num = num - 26;
+                    }
+                }
+                
+                sb.append((char)num);
+            }
+        }
+        return sb.toString();
+    }
+}
