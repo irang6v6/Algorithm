fe7506d39d7435b365611c6309805aaca25eda87
남이랑 <irang6v6@gmail.com>
2023-04-16 02:51:44 +0900

    Time: 1 ms (100.00%), Space: 50.4 MB (90.21%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,39 +1,18 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int[] answer = new int[n];
+        int[] res = new int[n];
+        res[0] = 1;
         
-        int zero = 0; // 0의 개수 저장
-        for(int i : nums){
-            if(i == 0){
-                zero++;
-            }
+        for(int i = 1; i < n; i++){
+            res[i] = res[i-1] * nums[i-1];
         }
         
-        if(zero > 1){ // 0이 2개 이상이면 모든 원소가 0
-            return answer;
-        } else if(zero == 1){ // 0이 1개면 그 원소 자리값만 계산하면 됨
-            int tmp = 1;
-            int idx = 0;
-            for(int i = 0; i < n; i++){
-                if(nums[i] != 0){
-                    tmp *= nums[i];
-                }else {
-                    idx = i;
-                }
-            }
-            answer[idx] = tmp;
-            return answer;
-        } else { // 0이 없음
-            int multiple = 1;
-            for(int i = 0; i < n; i++){
-                multiple *= nums[i];
-            }
-            for(int j = 0; j < n; j++){
-                answer[j] = multiple / nums[j];
-            }
-            
-            return answer;
+        int right = 1;
+        for(int i = n - 1; i >= 0; i--){
+            res[i] *= right;
+            right *= nums[i]; 
         }
+        return res;
     }
 }
