3049592988f9c79e76830d58addab27264299459
irang6v6 <111601joo@naver.com>
2023-08-22 20:17:01 +0900

    [unrated] Title: 코드 처리하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,16 @@
+def solution(code):
+    mode = 0
+    ret = ''
+    for idx in range(len(code)):
+        if mode == 0:
+            if code[idx] != "1" and idx % 2 == 0:
+                ret += code[idx]
+        if mode == 1:
+            if code[idx] != "1" and idx % 2 == 1:
+                ret += code[idx]
+        if code[idx] == "1":
+            if mode == 1:
+                mode = 0
+            else:
+                mode = 1
+    return ret if len(ret) > 0 else "EMPTY"
