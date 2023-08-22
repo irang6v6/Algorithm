a303aedfb28612032f8679fa0c3492733695e921
irang6v6 <111601joo@naver.com>
2023-08-22 20:55:13 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.py
@@ -1,10 +1,6 @@
 def solution(a, b, c):
     answer = 1
-    num = len(set([a,b,c]))
-    if num == 3:
-        num = 1
-    elif num == 1:
-        num = 3
+    num = 4 - len(set([a,b,c]))
         
     for i in range(1,num+1):
         answer *= a**i + b**i + c**i
