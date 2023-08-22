b8d3b9dd4f2fb3441c5120ce249b6dbbc66b618c
irang6v6 <111601joo@naver.com>
2023-08-22 21:17:28 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.py
@@ -0,0 +1,7 @@
+def solution(num_list):
+    length = len(num_list)
+    if num_list[length-1] > num_list[length-2]:
+        num_list.append(num_list[length-1]-num_list[length-2])
+    else:
+        num_list.append(num_list[length-1]*2)
+    return num_list
