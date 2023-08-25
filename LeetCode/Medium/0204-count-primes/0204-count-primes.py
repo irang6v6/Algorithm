a874e269c32b15e79d424704f88a7f280875f4fd
irang6v6 <111601joo@naver.com>
2023-08-26 03:13:22 +0900

    [Medium] Time: 1940 ms (76.17%), Memory: 105.8 MB (9.90%) - LeetHub

--- LeetCode/Medium/0204-count-primes/0204-count-primes.py
+++ LeetCode/Medium/0204-count-primes/0204-count-primes.py
@@ -4,16 +4,13 @@ class Solution:
     def countPrimes(self, n: int) -> int:
         arr = [1] * n
         
-        if n < 2:
+        if n <= 2:
             return 0
         else:
             arr[0], arr[1] = 0, 0
             
         for i in range(2,n):
             if arr[i] == 1:
-                j = 2
-                while i * j < n:
-                    arr[i * j] = 0
-                    j += 1
+                arr[i*i:n:i] = [0] * ((n - 1 - i * i) // i + 1)
         return sum(arr)
                     
