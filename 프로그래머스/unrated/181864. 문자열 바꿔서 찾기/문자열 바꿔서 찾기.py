76c2989fb39abe114e44c65a58a22f12f8201e35
irang6v6 <111601joo@naver.com>
2023-08-28 01:05:43 +0900

    [unrated] Title: 문자열 바꿔서 찾기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
@@ -1,5 +1,3 @@
 def solution(myString, pat):
-    myString = myString.replace("A","tmp")
-    myString = myString.replace("B","A")
-    myString = myString.replace("tmp","B")
+    myString = myString.replace("A","tmp").replace("B","A").replace("tmp","B")
     return int(pat in myString)
