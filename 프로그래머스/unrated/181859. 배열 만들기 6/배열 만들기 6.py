039cbda8d00a2d7d815c4363163a49726e41a99c
irang6v6 <111601joo@naver.com>
2023-08-28 19:03:09 +0900

    [unrated] Title: 배열 만들기 6, Time: 40.03 ms, Memory: 10.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.py
@@ -0,0 +1,13 @@
+def solution(arr):
+    stk = []
+    i = 0
+    while i < len(arr):
+        if not stk:
+            stk.append(arr[i])
+        else:
+            if stk[-1] == arr[i]:
+                stk = stk[:-1]
+            else:
+                stk.append(arr[i])
+        i += 1
+    return stk if stk else [-1]
