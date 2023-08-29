46405a90512a71fa54ea1629c8bdc6c60d9d0454
irang6v6 <111601joo@naver.com>
2023-08-29 16:48:34 +0900

    [level 1] Title: 폰켓몬, Time: 0.39 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,5 +1,2 @@
-from collections import Counter
 def solution(nums):
-    kind = len(set(nums))
-    avail = len(nums)/2
-    return avail if avail <= kind else kind
+    return min(len(set(nums)),len(nums)/2)
